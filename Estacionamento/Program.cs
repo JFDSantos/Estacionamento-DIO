bb76faa89ba0a2bc757822573cd280500e2afaeb
Jeferson Santos <jeeh71ferreira@gmail.com>
2024-01-03 17:32:11 -0300

    Adicionar arquivos de projeto.

---
+++ Estacionamento/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Estacionamento;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+float valorInicial;
+float valorHora;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+Console.WriteLine("Digite o preço inicial: ");
+
+valorInicial = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora: ");
+
+valorHora = float.Parse(Console.ReadLine());
+
+Estacionamento
