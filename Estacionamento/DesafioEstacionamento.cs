04c8943bb9fdf12c32d667ca1673291e6d7349b1
Jeferson Santos <jeeh71ferreira@gmail.com>
2024-01-04 09:04:18 -0300

    Finalizando projeto

---
+++ Estacionamento/DesafioEstacionamento.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioEstacionamento
+{
+    public class Estacionamento
+    {
+        private decimal valorInicial = 0;
+        private decimal valorHora = 0;
+        private List<string> veiculos = new List<string>();
+        public Estacionamento(decimal valorInicial, decimal valorHora) {
+
+            this.valorInicial = valorInicial;
+            this.valorHora = valorHora;
+        }
+
+        public void cadastrarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar: ");
+            string placa = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(placa)) 
+            {
+                Console.Clear();
+                Console.WriteLine("Digite uma placa válida!\n");
+                cadastrarVeiculo();
+            }
+            else
+            {
+                Console.Clear();
+
+                if (veiculos.Any(x => x.ToUpper() == placa.ToUpper())){
+                    Console.WriteLine("O veículo já se encontra estacionado!\n");
+                }
+                else
+                {
+                    veiculos.Add(placa);
+                }
+                
+            }
+            
+        }
+        
+        public void listarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:\n");
+                foreach (string x in veiculos)
+                {
+                    Console.WriteLine($"Veículo: {x}");
+                }
+                Console.WriteLine("\n");
+            }
+            else
+            {
+                Console.WriteLine("Não há nenhum veículo estacionado!");
+            }
+        }
+
+        public void removerVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo, o qual deseja retirar do estacionamento: ");
+
+            string placa = Console.ReadLine();
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int horas = Convert.ToInt32(Console.ReadLine());
+
+                decimal valorTotal = valorInicial + valorHora * horas;
+
+                veiculos.Remove(placa);
+                Console.WriteLine($"\nO veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}\n");
+            }
+            else
+            {
+                Console.WriteLine("\nVeículo não encontrado!\n");
+            }
+        }
+    }
+}
--- Estacionamento/Program.cs
+++ Estacionamento/Program.cs
@@ -1,21 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Estacionamento;
+using DesafioEstacionamento;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        float valorInicial;
-        float valorHora;
+        decimal valorInicial;
+        decimal valorHora;
 
         Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
         Console.WriteLine("Digite o preço inicial: ");
 
-        valorInicial = float.Parse(Console.ReadLine());
+        valorInicial = Convert.ToDecimal(Console.ReadLine());
 
         Console.WriteLine("Agora digite o preço por hora: ");
 
-        valorHora = float.Parse(Console.ReadLine());
+        valorHora = Convert.ToDecimal(Console.ReadLine());
+
+        Estacionamento es = new Estacionamento(valorInicial, valorHora);
+
+        string opcao = string.Empty;
+        bool exibirMenu = true;
+
+        while (exibirMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a sua opção:");
+            Console.WriteLine("1 - Cadastrar veículo");
+            Console.WriteLine("2 - Remover veículo");
+            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("4 - Encerrar");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Clear();
+                    es.cadastrarVeiculo();
+                    break;
+
+                case "2":
+                    Console.Clear();
+                    es.removerVeiculo();
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    es.listarVeiculos();
+                    break;
+
+                case "4":
+                    exibirMenu = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
+            Console.WriteLine("\nPressione uma tecla para continuar");
+            Console.ReadLine();
+        }
+
+        Console.WriteLine("O programa se encerrou");
     }
 }
